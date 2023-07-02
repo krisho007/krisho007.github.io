44df6b5bdf92025b284bf1681ae9b4794e29b868
Krishna Kishor Kammaje <krishna.kammaje@yahoo.com>
2023-07-02 18:02:42 +0530

    simplified

--- Component.js
+++ Component.js
@@ -1,8 +1,7 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel"
-  ], function(UIComponent, JSONModel, ResourceModel) {
+  ], function(UIComponent, ResourceModel) {
     "use strict";
   
     return UIComponent.extend("myProfile.Component", {
@@ -13,16 +12,7 @@ sap.ui.define([
       init: function() {
         // call the base component's init function
         UIComponent.prototype.init.apply(this, arguments);
-  
-        // set data model
-        var oData = {
-          recipient: {
-            name: "John Doe"
-          }
-        };
-        var oModel = new JSONModel(oData);
-        this.setModel(oModel);
-  
+   
         // set i18n model
         var i18nModel = new ResourceModel({
           bundleName: "myProfile.i18n.i18n"
--- controller/ObjectPage.controller.js
+++ controller/ObjectPage.controller.js
@@ -5,6 +5,18 @@ sap.ui.define([
   
     return Controller.extend("myProfile.controller.Home", {
       // Controller methods and event handlers can be defined here
+      onLinkedInPress: function() {
+        //Open a new window with the LinkedIn profile
+        window.open("https://www.linkedin.com/in/kammaje/");
+      },
+      onSAPCommunityPress: function() {
+        //Open a new window with the SAP Community profile
+        window.open("https://people.sap.com/krishnakishor.kammaje2");
+      },
+      onMentorPress: function() {
+        //Open a new window with the SAP Community profile
+        window.open("https://community.sap.com/influencers?program=sapmentors&page=2");
+      }
     });
   });
   
